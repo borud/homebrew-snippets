dc4e2931df44092cefc3c819a0ba0106ec6b88eb
Bjørn Borud <borud@borud.no>
2026-05-31 19:47:26 +0200

    Update snippet to v0.3.0

--- Formula/snippet.rb
+++ Formula/snippet.rb
@@ -1,29 +1,29 @@
 class Snippet < Formula
   desc "CLI for managing snippets"
   homepage "https://s.borud.no"
-  version "v0.2.0"
+  version "v0.3.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/borud/snippets-releases/releases/download/v0.2.0/snippet_v0.2.0_darwin_arm64.tar.gz"
-      sha256 "3cf2eed6aa9c783472a39698d66667dfb20f8cd9c15ff38fc382c84112aea3ab"
+      url "https://github.com/borud/snippets-releases/releases/download/v0.3.0/snippet_v0.3.0_darwin_arm64.tar.gz"
+      sha256 "022dbb072cadb325c1aaca15f3efeaf2eb0dff2a205b9d9e2975462fff224084"
     end
     on_intel do
-      url "https://github.com/borud/snippets-releases/releases/download/v0.2.0/snippet_v0.2.0_darwin_amd64.tar.gz"
-      sha256 "ab7548042d130df4d7cf7d2c396b0aae7bd177ba7c6253de279cd776cbde84f0"
+      url "https://github.com/borud/snippets-releases/releases/download/v0.3.0/snippet_v0.3.0_darwin_amd64.tar.gz"
+      sha256 "3ccf8f9aebd92594f9e0381d5d86a5b9cc1ef95476f17b0d58bc5a4e3e36f5a2"
     end
   end
 
   on_linux do
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/borud/snippets-releases/releases/download/v0.2.0/snippet_v0.2.0_linux_arm64.tar.gz"
-        sha256 "8f46a028abe91bf62241f24bf49337d8afc4e620f5e81e191ade017838db1c63"
+        url "https://github.com/borud/snippets-releases/releases/download/v0.3.0/snippet_v0.3.0_linux_arm64.tar.gz"
+        sha256 "c72977531a66c652e31754f1942d4164aefa9514cf1d24eb3a344556e52c6e6b"
       end
     end
     on_intel do
-      url "https://github.com/borud/snippets-releases/releases/download/v0.2.0/snippet_v0.2.0_linux_amd64.tar.gz"
-      sha256 "5988bc0133c11fcd1aa3d4adc3d90c6829d001814e4eb1d4e16cda99ef56b19d"
+      url "https://github.com/borud/snippets-releases/releases/download/v0.3.0/snippet_v0.3.0_linux_amd64.tar.gz"
+      sha256 "11865843baf09faddc52d78b926ee215d345fdc723031aa32f9ecacc6cf30edd"
     end
   end
 
