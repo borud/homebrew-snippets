e283bd3f4961fbb7fbe10fb569c3250eddc4a9c9
Bjørn Borud <bjorn.borud@norbit.com>
2026-06-01 13:13:50 +0200

    Update snippet to v0.4.0

--- Formula/snippet.rb
+++ Formula/snippet.rb
@@ -1,29 +1,29 @@
 class Snippet < Formula
   desc "CLI for managing snippets"
   homepage "https://s.borud.no"
-  version "v0.3.1"
+  version "v0.4.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/borud/snippets-releases/releases/download/v0.3.1/snippet_v0.3.1_darwin_arm64.tar.gz"
-      sha256 "fa0026170f114702fe1301daa005968b5e125e204391ec7499e989a3b8bc0364"
+      url "https://github.com/borud/snippets-releases/releases/download/v0.4.0/snippet_v0.4.0_darwin_arm64.tar.gz"
+      sha256 "54c24c585087c3f3fe9ca359bfd1c9158afeac3a571f626d1164efdc9ae9bec7"
     end
     on_intel do
-      url "https://github.com/borud/snippets-releases/releases/download/v0.3.1/snippet_v0.3.1_darwin_amd64.tar.gz"
-      sha256 "f8599bce36191a1c7de2e5d701e01b38bb1aa394d8005030c52b68a2eb818b51"
+      url "https://github.com/borud/snippets-releases/releases/download/v0.4.0/snippet_v0.4.0_darwin_amd64.tar.gz"
+      sha256 "852b3ff35824e154644d01221816e76cc6431945b0f2a633ebc680f2562ea914"
     end
   end
 
   on_linux do
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/borud/snippets-releases/releases/download/v0.3.1/snippet_v0.3.1_linux_arm64.tar.gz"
-        sha256 "b8c1bc08b99d0a7bae398ace83895c7addde8dd5faa3e6369d1392e31c28f22a"
+        url "https://github.com/borud/snippets-releases/releases/download/v0.4.0/snippet_v0.4.0_linux_arm64.tar.gz"
+        sha256 "9cbf4c6e2f98b8a7f7c51254d9b1ed5949cc9f167c0abf98dee8a2940dd21aad"
       end
     end
     on_intel do
-      url "https://github.com/borud/snippets-releases/releases/download/v0.3.1/snippet_v0.3.1_linux_amd64.tar.gz"
-      sha256 "96e7cb53ddcafe0f2d9ef8dadc83d5e3c72821634c33d00a60970147730343ee"
+      url "https://github.com/borud/snippets-releases/releases/download/v0.4.0/snippet_v0.4.0_linux_amd64.tar.gz"
+      sha256 "62bf9b0a81b4f8713fbd03f8fbfc9138eb45e729a9b21b3568b217f608cd16c9"
     end
   end
 
