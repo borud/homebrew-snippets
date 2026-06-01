63791cec5123f0712de9936d6894ee4b29b7fce1
Bjørn Borud <bjorn.borud@norbit.com>
2026-06-01 16:26:19 +0200

    Update snippet to v0.4.2

--- Formula/snippet.rb
+++ Formula/snippet.rb
@@ -1,29 +1,29 @@
 class Snippet < Formula
   desc "CLI for managing snippets"
   homepage "https://s.borud.no"
-  version "v0.4.0"
+  version "v0.4.2"
 
   on_macos do
     on_arm do
-      url "https://github.com/borud/snippets-releases/releases/download/v0.4.0/snippet_v0.4.0_darwin_arm64.tar.gz"
-      sha256 "54c24c585087c3f3fe9ca359bfd1c9158afeac3a571f626d1164efdc9ae9bec7"
+      url "https://github.com/borud/snippets-releases/releases/download/v0.4.2/snippet_v0.4.2_darwin_arm64.tar.gz"
+      sha256 "3a83e27f463c7ed629ac84d38b9243d6145a136cb2fd7dfc30b1588817bbd655"
     end
     on_intel do
-      url "https://github.com/borud/snippets-releases/releases/download/v0.4.0/snippet_v0.4.0_darwin_amd64.tar.gz"
-      sha256 "852b3ff35824e154644d01221816e76cc6431945b0f2a633ebc680f2562ea914"
+      url "https://github.com/borud/snippets-releases/releases/download/v0.4.2/snippet_v0.4.2_darwin_amd64.tar.gz"
+      sha256 "d881074cdce58a7e242e9a74cdec8133604f333d925878582dc3b5f80075afd0"
     end
   end
 
   on_linux do
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/borud/snippets-releases/releases/download/v0.4.0/snippet_v0.4.0_linux_arm64.tar.gz"
-        sha256 "9cbf4c6e2f98b8a7f7c51254d9b1ed5949cc9f167c0abf98dee8a2940dd21aad"
+        url "https://github.com/borud/snippets-releases/releases/download/v0.4.2/snippet_v0.4.2_linux_arm64.tar.gz"
+        sha256 "dff1ba0ca1f759a14a342f437aa700e635a91b9a3c501e2a808944739b172516"
       end
     end
     on_intel do
-      url "https://github.com/borud/snippets-releases/releases/download/v0.4.0/snippet_v0.4.0_linux_amd64.tar.gz"
-      sha256 "62bf9b0a81b4f8713fbd03f8fbfc9138eb45e729a9b21b3568b217f608cd16c9"
+      url "https://github.com/borud/snippets-releases/releases/download/v0.4.2/snippet_v0.4.2_linux_amd64.tar.gz"
+      sha256 "3dbd94b16dc30851ddea56bd191af070fa43e2143622831af8c6949894e2ccdb"
     end
   end
 
